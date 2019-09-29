d59ea75aa65571ce00bb31c55ad4a845d3c6131d
Michael Pohily <mpohily@gmail.com>
2019-09-29 18:00:52 +0300

    add board init, __str__, add_two methods. add board.up

---
+++ board.py
@@ -0,0 +1,69 @@
+from random import choice
+
+class Board():
+    def __init__(self):
+        self.board = [['0', '0', '0', '0'], ['0', '0', '0', '0'], ['0', '0', '0', '0'], ['0', '0', '0', '0'] ]
+        self.blanks_left = list(range(16))
+        self.score = 0
+        self.add_two()
+        self.add_two()
+        
+        
+    def __str__(self):
+        lines = ['\nUp - Q   Down - A    Left - Z    Right - X']
+        lines.append(f'Score: {self.score}')
+        lines.append('------------------------------------------')
+        
+        for x in range(4):
+            line = []
+            for y in range(4):
+                line.append(self.board[x][y])
+            lines.append(' '.join(line))
+        return '\n'.join(lines)
+
+
+    def add_two(self):
+        if not self.blanks_left:
+            print('You lose!')
+        else:
+            taken = choice(self.blanks_left)
+            self.blanks_left.remove(taken)
+            
+            ver = taken // 4
+            hor = taken - 4 * ver
+            self.board[ver][hor] = '2'
+
+
+    def up(self):
+        for row in range(1, 4):
+            for column in range(4):
+                for check_row in range(row - 1, -1, -1):
+                    if self.board[check_row + 1][column] == '0':
+                        continue
+                    if self.board[check_row][column] == '0':
+                        self.board[check_row][column], self.board[check_row + 1][column] = self.board[check_row + 1][column], self.board[check_row][column]
+                        continue
+                    elif self.board[check_row][column] == self.board[check_row + 1][column]:
+                        self.board[check_row][column] = str(2 * int(self.board[check_row][column]))
+                        self.score += int(self.board[check_row + 1][column])
+                        self.board[check_row + 1][column] = '0'
+                        break
+
+
+    def down(self):
+        pass
+
+
+    def left(self):
+        pass
+
+
+    def right(self):
+        pass
+            
+
+a = Board()
+print(a)
+print(a.blanks_left)
+a.up()
+print(a)
